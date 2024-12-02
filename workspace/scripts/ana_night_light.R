7e33eed02a8da0ffbdd06ea2350b4c7d10dbeafb
David Beauchesne <david.beauchesne@uqar.ca>
2024-12-02 12:29:06 -0500

    first version of report section on threat layers

--- workspace/scripts/ana_night_light.R
+++ workspace/scripts/ana_night_light.R
@@ -13,12 +13,42 @@ ana_night_light_monthly <- function(input_files, output_path) {
   input_files <- input_files[!basename(input_files) %in% c("grid.tif", "aoi.gpkg")] |>
     dir(full.names = TRUE, pattern = ".tif")
 
+  # Get necessary data to mask only along the coast
+  # Coastal outline
+  fetch_data <- function(url) {
+    # Temporary file
+    tmp <- tempfile(fileext = ".json")
+
+    # Download file
+    curl::curl_download(url, tmp)
+
+    # Import
+    sf::st_read(tmp, quiet = TRUE)
+  }
+
+  coastlines <- c(
+    "https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_USA_0.json",
+    "https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_CAN_0.json",
+    "https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_GRL_0.json"
+  ) |>
+    lapply(fetch_data)
+
+  terre <- sf::st_union(coastlines[[1]], coastlines[[2]]) |>
+    sf::st_union(coastlines[[3]]) |>
+    sf::st_transform(32198) |> # To get units in meters
+    smoothr::fill_holes(threshold = units::set_units(1000, km^2)) |>
+    sf::st_buffer(1000) |>
+    sf::st_transform(4326)
+
+  # Coastline
+  coast <- sf::st_intersection(terre, aoi)
+
   # Data
   for (i in seq_len(length(input_files))) {
     # Resample
     dat <- terra::rast(input_files[i]) |>
       terra::resample(grid) |>
-      terra::mask(aoi)
+      terra::mask(coast)
 
     # Export
     current_month <- basename(input_files[i]) |>
