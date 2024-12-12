73d62ac515e9ca112115b7b99bb4ab6d426ea51b
Kevin Cazelles <kevin.cazelles@insileco.io>
2024-12-12 12:49:41 -0500

    review report kc [skip actions]

--- R/fig_aoi.R
+++ R/fig_aoi.R
@@ -20,13 +20,14 @@ fig_aoi <- function(aoi) {
   # gl <- sf::st_read(here::here("project-data/basemap/greenland.gpkg"), quiet = TRUE)
   bbox <- sf::st_bbox(aoi)
   par(family = "serif", mar = c(.5, .5, .5, .5))
-  graphicsutils::plot0(x = c(bbox$xmin, bbox$xmax), y = c(bbox$ymin, bbox$ymax))
   plot(
     sf::st_geometry(aoi),
     lwd = 2,
     border = "#000000BB",
     col = "#376e7d88",
-    add = TRUE
+    xlim = c(bbox$xmin, bbox$xmax), 
+    ylim = c(bbox$ymin, bbox$ymax),
+    axes = FALSE
   )
   # plot(sf::st_geometry(can), lwd = .5, col = "#00000088", add = TRUE)
   # plot(sf::st_geometry(usa), lwd = .5, col = "#00000088", add = TRUE)
